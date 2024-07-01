de75e4a388a46cd8e6ee43145e32615d35de4995
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-07-01 16:39:16 +0200

    declare reactive values

--- shinyapp/server.R
+++ shinyapp/server.R
@@ -21,10 +21,15 @@ shinyServer(
     
     # -- communication objects
     r$header <- NULL
+    r$dataset <- NULL
+    r$cols_before_candidate <- NULL
+    r$cols_candidate <- NULL
+    
     
     # -- launch module servers
     data_manager_Server(id = "data", r, path)
     data_model_Server(id = "dm", r, path)
+    general_result_Server(id = "general", r, path)
 
   }
 )
